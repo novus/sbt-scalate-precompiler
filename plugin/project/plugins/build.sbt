3ad6691ed8d302a6e0b06852b94e19c11c6cd0bd
Chris Lewis <chris@thegodcode.net>
2011-09-20 00:18:22 -0400

    initial plugin

---
+++ plugin/project/plugins/build.sbt
@@ -0,0 +1,5 @@
+//resolvers += "Typesafe Repo" at "http://repo.typesafe.com/typesafe/releases/"
+
+resolvers += "Novus Snapshots Repository" at "http://repo.novus.com/snapshots/"
+
+libraryDependencies += "pragmagica" %% "scalate-precompiler" % "0.0.10"
---
+++ plugin/src/main/scala/ScalatePlugin.scala
@@ -0,0 +1,28 @@
+import sbt._
+import Keys._
+
+import java.io.File
+
+object ScalatePlugin extends Plugin {
+  val Scalate = config("scalate") extend(Compile)
+  val importsFile = SettingKey[Option[File]]("imports-file", "Imports inserted into compiled template.")
+  val generatedDirectory = SettingKey[File]("generated-directory", "Output directory for precompiled templates.")
+  val templateRoots = SettingKey[Seq[File]]("template-roots", "Directorires containing Scalate templates.")
+  val precompile = TaskKey[File]("precompile", "Precompile Scalate templates.")
+  def precompileScalate(output: File, templateRoots: Seq[File], imports: File) = {
+    println("%s, %s, %s".format(templateRoots.map(_.toString).toList, output.toString, imports.toString))
+    try {
+    pragmagica.scalate.Generator.precompile(templateRoots.map(_.toString).toList, output.toString, imports.toString)
+    } catch {
+      case e => e.printStackTrace
+    }
+    output
+  }
+  
+  lazy val pluginSettings = inConfig(Scalate)(Seq(
+    templateRoots <<= (resourceDirectory in Compile) { rd => Seq(rd / "templates") },
+    importsFile <<= (resourceDirectory in Compile) { rd => Some(rd / "imports.txt") },
+    generatedDirectory <<= (sourceManaged in Compile) { _ / "gen" },
+    precompile <<= (sourceManaged, templateRoots, importsFile) map { (g, roots, imports) => precompileScalate(g, roots, imports.get) }
+  ))
+}
