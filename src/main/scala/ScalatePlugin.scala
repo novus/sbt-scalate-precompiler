39c2a212bc00e416855b933bb1fc29c3667b4c7d
Chris Lewis <chris@thegodcode.net>
2011-09-22 17:58:00 -0400

    PathFinder ftw

--- src/main/scala/ScalatePlugin.scala
+++ src/main/scala/ScalatePlugin.scala
@@ -18,8 +18,8 @@ object ScalatePlugin extends Plugin {
     val Config              = config("scalate") extend(Compile)
     val importsFile         = SettingKey[Option[File]]  ("imports-file", "Imports inserted into compiled template.") in Config
     val generatedDirectory  = SettingKey[File]          ("generated-directory", "Output directory for precompiled templates.") in Config
-    val templateRoots       = SettingKey[Seq[File]]     ("template-roots", "Directorires containing Scalate templates.") in Config
-    val precompile          = TaskKey[Seq[File]]        ("precompile", "Precompile Scalate templates.") in Config
+    val templateRoots       = SettingKey[PathFinder]     ("template-roots", "Directorires containing Scalate templates.") in Config
+    val precompile          = TaskKey[PathFinder]        ("precompile", "Precompile Scalate templates.") in Config
   
     lazy val settings = Seq(
       templateRoots       <<= (resourceDirectory in Compile) { _  / "templates" :: Nil },
@@ -31,7 +31,7 @@ object ScalatePlugin extends Plugin {
     )
   }
   
-  def precompileScalate(output: File, templateRoots: Seq[File], imports: File) = {
-    pragmagica.scalate.Generator.precompile(templateRoots.map(_.toString).toList, output.toString, imports.toString)
+  def precompileScalate(output: File, templateRoots: PathFinder, imports: File) = {
+    pragmagica.scalate.Generator.precompile(templateRoots.getPaths.toList, output.toString, imports.toString)
   }
 }
