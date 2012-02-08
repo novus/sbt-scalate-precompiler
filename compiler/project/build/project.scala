f19aaf7ba01cabb535e4de7761f4e328c12fe355
Chris Lewis <chris@thegodcode.net>
2012-02-08 00:14:13 -0500

    scala 2.9.1, scalate 1.5.3

--- compiler/project/build/project.scala
+++ compiler/project/build/project.scala
@@ -2,7 +2,7 @@ import sbt._
 
 class PrecompilerProject(info: ProjectInfo) extends DefaultProject(info) {
 
-  val ScalateVersion = "1.4.1"
+  val ScalateVersion = "1.5.3"
   val scalate_core = "org.fusesource.scalate"  % "scalate-core"  % ScalateVersion
   val scalate_util = "org.fusesource.scalate"  % "scalate-util"  % ScalateVersion
   val slf4j        = "org.slf4j"               % "slf4j-jdk14"   % "1.6.1"
--- plugin/project/build/project.scala
+++ plugin/project/build/project.scala
@@ -1,7 +1,7 @@
 import sbt._
 
 class PrecompilerPluginProject(info: ProjectInfo) extends PluginProject(info) {
-  val precompiler = "pragmagica" % "scalate-precompiler_2.8.1" % "0.0.8"
+  val precompiler = "pragmagica" % "scalate-precompiler_2.9.1" % "0.0.11"
   override def managedStyle = ManagedStyle.Maven
   val publishTo = Resolver.sftp("repobum", "repobum", "/home/public/%s".format(
     if (projectVersion.value.toString.endsWith("-SNAPSHOT")) "snapshots"
